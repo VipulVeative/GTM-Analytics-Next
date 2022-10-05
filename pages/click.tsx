9855408b6118888298374269138cd048501a4866
Vipul Pathak <vipul.pathak@Veative.local>
2022-10-05 13:50:05 +0530

    click event

--- pages/click.tsx
+++ pages/click.tsx
@@ -3,12 +3,20 @@ import Head from 'next/head';
 
 export default function clickPage(){
 
+    const eventHandle=()=>{
+        console.log("hiii");
+        gtag('event', 'click', {'pageName': 'Click.tsx','time':new Date()});
+   
+       }
+
     return (
         <>
           <Head>
                 <title>Click page</title>
             </Head>
         <div>This is click page</div>
+        <button onClick={eventHandle}>Event button</button>
+
         </>
     )
 
